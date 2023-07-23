1aafeb614bc7d9cdffa78fd1885917b259df7c68
William Ruiz <iam@waruiz.com>
2023-07-23 14:58:07 -0700

    create simple GET by ProductId

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import express from "express";
 
 const server = express();
 const PORT = 3000;
+
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}.`);
+});
+
+server.get("/health", (req, res) => {
+  const dateTimeNow = new Date().toISOString();
+  res.send(`Alive on ${dateTimeNow}.`);
+});
+
+server.get("/products/:productId", (req, res) => {
+  const productId = req.params.productId;
+  res.send(`Requested product ${productId}`);
 });
